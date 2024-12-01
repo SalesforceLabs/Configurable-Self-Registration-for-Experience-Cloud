20eeec2124282d5d9a99db50ebd19e017f926106
Mike Cleaver <mike.cleaver@lavaboxconsulting.com>
2024-12-02 10:58:52 +1300

    purely formatting with prettier

--- Configurable Self Registration LWC/Custom Registration/force-app/main/default/lwc/customPropertyEditorVerificationMethods/customPropertyEditorVerificationMethods.js
+++ Configurable Self Registration LWC/Custom Registration/force-app/main/default/lwc/customPropertyEditorVerificationMethods/customPropertyEditorVerificationMethods.js
@@ -1,32 +1,30 @@
-import { LightningElement, wire, api } from 'lwc';
-import checkSMSLicence from '@salesforce/apex/SiteUtilities.checkSMSLicence';
+import { LightningElement, wire, api } from "lwc";
+import checkSMSLicence from "@salesforce/apex/SiteUtilities.checkSMSLicence";
 
 export default class customPropertyEditorVerificationMethods extends LightningElement {
-    
-    @api value;
-    
-    //Default is Email, which is not licence based.
-    verificationOptions = [
-        { label: 'Email', value: 'Email' },
-    ];
+  @api value;
 
-    @wire(checkSMSLicence)
-    wiredRecordTypes({error, data}) {
-        if(data) {
-            this.verificationOptions = [
-                { label: 'Email', value: 'Email' },
-                { label: 'SMS', value: 'SMS' },
-            ];
-        }
-    }
+  //Default is Email, which is not licence based.
+  verificationOptions = [{ label: "Email", value: "Email" }];
 
-    get options() {
-        return this.verificationOptions;
+  @wire(checkSMSLicence)
+  wiredRecordTypes({ error, data }) {
+    if (data) {
+      this.verificationOptions = [
+        { label: "Email", value: "Email" },
+        { label: "SMS", value: "SMS" }
+      ];
     }
+  }
 
-    handleChange(event) {
-        this.value = event.detail.value;
-        this.dispatchEvent(new CustomEvent("valuechange", 
-        {detail: {value: this.value}}));
-    }
+  get options() {
+    return this.verificationOptions;
+  }
+
+  handleChange(event) {
+    this.value = event.detail.value;
+    this.dispatchEvent(
+      new CustomEvent("valuechange", { detail: { value: this.value } })
+    );
+  }
 }
