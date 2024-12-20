5f7239e1e4bb2cf0b79842250228a3f159721e8d
jlowe-SFDC <jamie.lowe@salesforce.com>
2024-12-20 15:12:16 +0000

    Fix up repository to remove nested folder structure causing gitignore tracking issues.

---
+++ force-app/main/default/lwc/customPropertyEditorCreateType/customPropertyEditorCreateType.js
@@ -0,0 +1,18 @@
+import { LightningElement, api } from "lwc";
+
+export default class customPropertyEditorCreateType extends LightningElement {
+	@api value;
+
+	get options() {
+		return [
+			{ label: "", value: "" },
+			{ label: "Contact", value: "Contact" },
+			{ label: "Person Account", value: "Person Account" }
+		];
+	}
+
+	handleChange(event) {
+		this.value = event.detail.value;
+		this.dispatchEvent(new CustomEvent("valuechange", { detail: { value: this.value } }));
+	}
+}
